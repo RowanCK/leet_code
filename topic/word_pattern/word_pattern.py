15bf20782be3c4302e9e464384b667eb7b583e01
Rowan <cek2llm@gmail.com>
2025-09-29 09:28:31 -0600

    Add: solution 2 for word_pattern

--- topic/word_pattern/word_pattern.py
+++ topic/word_pattern/word_pattern.py
@@ -1,7 +1,24 @@
-from itertools import zip_longest
-
-
 class Solution:
     def wordPattern(self, pattern: str, s: str) -> bool:
-        s = s.split()
-        return (len(set(pattern)) == len(set(s)) == len(set(zip_longest(pattern, s))))
+        words = s.split()
+
+        if len(pattern) != len(words):
+            return False
+
+        exist_word = {}
+        exist_pattern = {}
+
+        for idx, c in enumerate(pattern):
+            if c in exist_pattern and exist_pattern[c] != words[idx]:
+                return False
+            if words[idx] in exist_word and exist_word[words[idx]] != c:
+                return False
+            exist_pattern[c] = words[idx]
+            exist_word[words[idx]] = c
+
+        return True
+
+class Solution2:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        words = s.split()
+        return (len(set(pattern)) == len(set(words)) == len(set(zip(pattern, words))))
