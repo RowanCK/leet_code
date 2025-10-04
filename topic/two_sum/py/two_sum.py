d4aa8691a56bebf56944f6105284ba58fe2086c2
Rowan <cek2llm@gmail.com>
2025-10-04 14:35:53 -0600

    Eenhance: two_sum format

--- topic/two_sum/py/two_sum.py
+++ topic/two_sum/py/two_sum.py
@@ -4,10 +4,11 @@ from typing import List
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         exist = {}
+
         for idx, value in enumerate(nums):
             remain = target - value
-
             if remain in exist:
                 return [exist[remain], idx]
-
             exist[value] = idx
+
+        return []
