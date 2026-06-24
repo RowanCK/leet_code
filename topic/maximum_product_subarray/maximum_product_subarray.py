db736fe2eb0f38d920c848bde04414cce837b82d
Rowan <cek2llm@gmail.com>
2026-06-24 14:44:54 +0800

    Add: maximum_product_subarray

---
+++ topic/maximum_product_subarray/maximum_product_subarray.py
@@ -0,0 +1,18 @@
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        
+        current_max = current_min = max_product = nums[0]
+        
+        for i in range(1, len(nums)):
+            if nums[i] < 0:
+                current_max, current_min = current_min, current_max
+            current_max = max(nums[i], current_max * nums[i])
+            current_min = min(nums[i], current_min * nums[i])
+            max_product = max(max_product, current_max)
+
+        return max_product
